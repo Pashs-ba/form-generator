65e046779b16d22118a29c42df4ac4ef39d17859
Pashs-ba <pashs.ba@gmail.com>
2024-02-09 12:42:01 +0300

    make another example

--- src/App.tsx
+++ src/App.tsx
@@ -4,62 +4,69 @@ import { ElementType, Form } from "../lib/main.ts";
 function App() {
 
   return (
-    <Form
-      onButtonClick={(data) => { console.log(data) }}
-      buttonText="Отправить"
-      elements={[
-        {
-          label: "one",
-          name: "one",
-          type: ElementType.TEXT_INPUT,
-          properties: {
-            type: "text"
-          }
-        },
-        {
-          label: "two",
-          name: "two",
-          type: ElementType.TEXT_INPUT,
-          properties: {
-            type: "text"
-          }
-        },
-        {
-          label: "select",
-          name: "select",
-          type: ElementType.SELECT,
-          
-          properties: {
-            multiple: true,
-            options: [
-              { value: "one", label: "one" },
-              { value: "two", label: "two" },
-              { value: "three", label: "three" },
-            ]
-          }
-        },
-        {
-          label: "checkbox",
-          name: "checkbox",
-          type: ElementType.CHECKBOX,
-          properties: {}
-        },
-        {
-          label: "radio",
-          name: "radio",
-          required: true,
-          type: ElementType.RADIO,
-          invalidText: "Please select one",
-          properties: {
-            options: [
-              "one",
-              "two",
-              "three",
-            ]
-          }
-        },
-      ]}
-    />
+    <div
+      style={{ height: "100vh" }}
+      className={"d-flex justify-content-center align-items-center"} >
+      <div className="col-5">
+        <div className="card ">
+          <div className="card-body">
+            <Form
+              title="Регистрация"
+              buttonText="Зарегистрироваться"
+              defaultInvalidText="Пожалуйста заполните поле"
+              onButtonClick={(data) => {
+                console.log(data)
+              }}
+              elements={[
+                {
+                  type: ElementType.TEXT_INPUT,
+                  label: "Имя",
+                  name: "name",
+                  required: true,
+                  properties: {
+                  },
+                },
+                {
+                  type: ElementType.TEXT_INPUT,
+                  label: "Фамилия",
+                  name: "surname",
+                  required: true,
+                  properties: {
+                  },
+                },
+                {
+                  type: ElementType.TEXT_INPUT,
+                  label: "Электропочта",
+                  name: "email",
+                  required: true,
+                  properties: {
+                    type: "email",
+                  },
+                },
+                {
+                  type: ElementType.TEXT_INPUT,
+                  label: "Пароль",
+                  name: "password",
+                  required: true,
+                  properties: {
+                    type: "password",
+                  },
+                },
+                {
+                  type: ElementType.TEXT_INPUT,
+                  label: "Повторите пароль",
+                  name: "password2",
+                  required: true,
+                  properties: {
+                    type: "password",
+                  },
+                },
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
