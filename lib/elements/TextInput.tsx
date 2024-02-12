18ca9b1d66ae7f5f144fd0755980335c02e9258d
Pashs-ba <pashs.ba@gmail.com>
2024-02-12 19:34:19 +0300

    small improvement

--- lib/elements/TextInput.tsx
+++ lib/elements/TextInput.tsx
@@ -7,14 +7,18 @@ type _TextInputProps = TextInputProps & {
     onChange: (data: string) => void
     defaultValue?: string
     required?: boolean
+    min?: number
+    max?: number
 }
-export default function TextInput({ onChange, type, defaultValue, required }: Readonly<_TextInputProps>) {
+export default function TextInput({ onChange, type, defaultValue, required, min, max }: Readonly<_TextInputProps>) {
     const [value, setValue] = React.useState<string>(defaultValue ?? "")
     return (
         <input
             type={type}
             required={required}
             className={"form-control"}
+            min={min}
+            max={max}
             value={value}
             onChange={(e: React.FormEvent<HTMLInputElement>) => { 
                 onChange(e.currentTarget.value) 
